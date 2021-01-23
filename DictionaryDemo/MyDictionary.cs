82d0cb9b7fe3cedf7c6366802906e19124308efd
SEVGİ <SEVG@DESKTOP-FLEASTC>
2021-01-23 16:01:36 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] key_array;
+        Tvalue[] value_array;
+
+        Tkey[] t_keyArray;
+        Tvalue[] t_valueArray;
+        public MyDictionary()
+        {
+            key_array = new Tkey[0];
+            value_array = new Tvalue[0];
+        }
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] t_keyArray = key_array;
+            Tvalue[] t_valueArray = value_array;
+            
+            key_array = new Tkey[key_array.Length + 1];
+            value_array = new Tvalue[value_array.Length + 1];
+            
+            for (int i = 0; i < t_keyArray.Length; i++)
+            {
+                key_array[i] = t_keyArray[i];
+                value_array[i] = t_valueArray[i];
+            }
+            
+            key_array[key_array.Length - 1] = key;
+            value_array[value_array.Length - 1] = value;
+        }
+        
+        public int Count
+        {
+            get { return key_array.Length; }
+        }
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Halit");
+            myDictionary.Add(2, "Yıldız");
+            myDictionary.Add(3, "Şahika");
+            
+            Console.WriteLine(myDictionary.Count);
+            
+        }
+    }
+}
